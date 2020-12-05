7c357b534cd7531a32dfd319c4f9b86418bb33b1
c-fowle <c_fowle@neuralpocket.com>
2020-12-05 15:54:57 +0900

    Add placeholder classes

---
+++ AoC/2020/Day08.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AoC.Common;
+using AoC.Common.Attributes;
+using AoC.Common.ExtensionMethods;
+
+namespace AoC._2020
+{
+    [Year(2020)]
+    [Day(8)]
+    public class Day08: Puzzle
+    {
+        private string[] ParseInput(string input) => input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        protected override string Part1(string input)
+        {
+            var parsedInput = ParseInput(input);
+            throw new NotImplementedException();
+        }
+        protected override string Part2(string input)
+        {
+            var parsedInput = ParseInput(input);
+            throw new NotImplementedException();
+        }
+    }
+}
+
