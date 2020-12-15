1eb8d29b746a4d562767150bcf3691b474b462e6
c-fowle <c_fowle@neuralpocket.com>
2020-12-15 14:19:31 +0900

    Solution 2020 Day 15

--- AoC/2020/Day15.cs
+++ AoC/2020/Day15.cs
@@ -12,19 +12,60 @@ namespace AoC._2020
 {
     [Year(2020)]
     [Day(15)]
+    [Test("0,3,6", "436", "175594")]
     public class Day15: Puzzle
     {
-        private string[] ParseInput(string input) => input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        private int[] ParseInput(string input) => input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(i => int.Parse(i)).ToArray();
 
         protected override string Part1(string input)
         {
             var parsedInput = ParseInput(input);
-            throw new NotImplementedException();
+
+            var previousNumbers = new Dictionary<int, int>();
+            var turn = 1;
+            var lastNumber = parsedInput[0];
+
+            while (turn < 2020)
+            {
+                int thisNumber;
+                
+                if (turn < parsedInput.Length) thisNumber = parsedInput[turn];
+                else if (previousNumbers.ContainsKey(lastNumber)) thisNumber = (turn) - previousNumbers[lastNumber];
+                else thisNumber = 0;
+
+                if (!previousNumbers.ContainsKey(lastNumber)) previousNumbers.Add(lastNumber, turn);
+                else previousNumbers[lastNumber] = turn;
+
+                lastNumber = thisNumber;
+                turn++;
+            }
+
+            return lastNumber.ToString();
         }
         protected override string Part2(string input)
         {
             var parsedInput = ParseInput(input);
-            throw new NotImplementedException();
+
+            var previousNumbers = new Dictionary<int, int>();
+            var turn = 1;
+            var lastNumber = parsedInput[0];
+
+            while (turn < 30000000)
+            {
+                int thisNumber;
+
+                if (turn < parsedInput.Length) thisNumber = parsedInput[turn];
+                else if (previousNumbers.ContainsKey(lastNumber)) thisNumber = (turn) - previousNumbers[lastNumber];
+                else thisNumber = 0;
+
+                if (!previousNumbers.ContainsKey(lastNumber)) previousNumbers.Add(lastNumber, turn);
+                else previousNumbers[lastNumber] = turn;
+
+                lastNumber = thisNumber;
+                turn++;
+            }
+
+            return lastNumber.ToString();
         }
     }
 }
